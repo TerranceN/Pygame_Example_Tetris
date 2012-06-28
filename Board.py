540c97c00c8488ca1dad00b28c33940e16021358
Terrance Niechciol <TNiechciol@gmail.com>
2012-06-28 11:03:32 -0400

    Filled rows are now removed when a shape is placed.

--- Board.py
+++ Board.py
@@ -1,4 +1,5 @@
 import pygame
+import copy
 
 class Block:
     image = None
@@ -40,6 +41,19 @@ class Board:
                 if not shape[i][j] == None:
                     pos = (position[0] + j, position[1] + i)
                     self.board[pos[1]][pos[0]] = shape[i][j]
+        for i in range(len(shape)):
+            isNotEmpty = False
+            for j in range(len(shape[i])):
+                if not shape[i][j] == None:
+                    isNotEmpty = True
+            if isNotEmpty:
+                self.checkForFilledRow(position[1] + i)
+    def checkForFilledRow(self, row):
+        for j in range(len(self.board[row])):
+            if self.board[row][j] == None: return
+        for currentRow in self.__reverseList(range(row)):
+            for j in range(len(self.board[currentRow])):
+                self.board[currentRow+1][j] = self.board[currentRow][j]
     def draw(self, display):
         for i in range(self.boardHeight):
             for j in range(self.boardWidth):
@@ -55,3 +69,7 @@ class Board:
         for block in self.blocks:
             if block.name == name:
                 return block
+    def __reverseList(self, lst):
+        newLst = copy.deepcopy(lst)
+        newLst.reverse()
+        return newLst
